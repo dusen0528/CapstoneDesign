748041f136e7b26820b11bef12a7091904f5b496
dusen0528 <97283087+dusen0528@users.noreply.github.com>
2024-06-12 08:25:23 +0900

    Updata

--- Back-end/src/dataInput.js
+++ Back-end/src/dataInput.js
@@ -1,42 +1,47 @@
-const fs = require('fs');
-const csv = require('csv-parser');
-const { MongoClient } = require('mongodb');
+const fs = require("fs");
+const csv = require("csv-parser");
+const { MongoClient } = require("mongodb");
 
-const uri = 'mongodb://localhost:27017';
-const dbName = 'Project';
-const collectionName = 'shelter';
-const csvFilePath = './src/data/shelters.csv';
+const uri = "mongodb://localhost:27017";
+const dbName = "Project";
+const collectionName = "shelter";
+const csvFilePath = "data/shelters.csv";
 
 async function main() {
-    const client = new MongoClient(uri);
-    try {
-        await client.connect();
-        console.log('Connected successfully to MongoDB');
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
-        const shelters = [];
-        fs.createReadStream(csvFilePath)
-            .pipe(csv())
-            .on('data', (data) => {
-                // 위도와 경도를 기반으로 location 필드를 GeoJSON 형식으로 생성
-                const location = {
-                    type: "Point",
-                    coordinates: [parseFloat(data['경도(EPSG4326)']), parseFloat(data['위도(EPSG4326)'])] // 주의: GeoJSON은 [경도, 위도] 순서입니다.
-                };
-                // 원래 데이터에 location 필드 추가
-                shelters.push({ ...data, location });
-            })
-            .on('end', async () => {
-                // 읽은 데이터를 MongoDB에 삽입
-                await collection.insertMany(shelters);
-                console.log('CSV file has been successfully imported into MongoDB with GeoJSON location');
-                await client.close();
-            });
-    } catch (err) {
-        console.error('An error occurred:', err);
+    const shelters = [];
+    fs.createReadStream(csvFilePath)
+      .pipe(csv())
+      .on("data", (data) => {
+        // 위도와 경도를 기반으로 location 필드를 GeoJSON 형식으로 생성
+        const location = {
+          type: "Point",
+          coordinates: [
+            parseFloat(data["경도(EPSG4326)"]),
+            parseFloat(data["위도(EPSG4326)"]),
+          ], // 주의: GeoJSON은 [경도, 위도] 순서입니다.
+        };
+        // 원래 데이터에 location 필드 추가
+        shelters.push({ ...data, location });
+      })
+      .on("end", async () => {
+        // 읽은 데이터를 MongoDB에 삽입
+        await collection.insertMany(shelters);
+        console.log(
+          "CSV file has been successfully imported into MongoDB with GeoJSON location"
+        );
         await client.close();
-    }
+      });
+  } catch (err) {
+    console.error("An error occurred:", err);
+    await client.close();
+  }
 }
 
 main().catch(console.error);
--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const cors = require('cors')
-const mongoose = require('mongoose');
-const connectDB = require('./connectDB'); 
-
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./connectDB");
 
 const app = express();
-require('dotenv').config(); // 환경 변수
+// require("dotenv").config(); // 맥 환경 변수
+require("dotenv").config({ path: "../.env" }); // 윈도우
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
-const smsRouter = require('./api/shelters/sms');  // 경로가 정확한지 확인
-app.use('/sms', smsRouter);
+const smsRouter = require("./api/shelters/sms"); // 경로가 정확한지 확인
+app.use("/sms", smsRouter);
 
 // 카카오 로그인 라우트 연결
-const kakaoAuthRouter = require('./api/auth/kakao');
-app.use('/api/auth/kakao', kakaoAuthRouter);
-
+const kakaoAuthRouter = require("./api/auth/kakao");
+app.use("/api/auth/kakao", kakaoAuthRouter);
 
 // 대피소 라우트 연결
-const sheltersRouter = require('./api/shelters/shelters');
-app.use('/api/shelters', sheltersRouter);
-
+const sheltersRouter = require("./api/shelters/shelters");
+app.use("/api/shelters", sheltersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
